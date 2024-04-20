47702a9f4f2aa999b47e7aa44f50f15e353392e8
YuraL0ns <yuralons@yandex.ru>
2024-04-20 19:21:34 +0300

    Small updates (F629)

--- resources/views/tmp/sait/page/vacansy/main.blade.php
+++ resources/views/tmp/sait/page/vacansy/main.blade.php
@@ -1,12 +1,18 @@
 @extends('tmp.main.default')
 @section('content')
 
-    @foreach($vacansies as $vacancy)
-        <li>
-            <a href="{{route('sait.vacancy.show', $vacancy->vacancy_alias)}}">
-                {{$vacancy->vacancy_title}}
-            </a>
-        </li>
-    @endforeach
+    <div class="vacancy_page">
+        <div class="vacancy-container">
+            <div class="vacancy-box">
+                @foreach($vacansies as $vacancy)
+                    <a class="vacancy-item" href="{{route('sait.vacancy.show', $vacancy->vacancy_alias)}}">
+                        <img class="vacancy-item__images" src="{{$vacancy->vacancy_images}}" alt="">
+                        <h3 class="vacancy-item__header">{{$vacancy->vacancy_title}}</h3>
+                        <p class="vacancy-item__look">Подробнее</p>
+                    </a>
+                @endforeach
+            </div>
+        </div>
+    </div>
 
 @stop
