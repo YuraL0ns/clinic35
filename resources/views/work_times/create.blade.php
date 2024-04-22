76e0dbd4b096775190d56096013607418bc1dcf0
YuraL0ns <yuralons@yandex.ru>
2024-04-22 22:30:50 +0300

    Update RE:(Add Work Times - Admin)2

--- resources/views/work_times/create.blade.php
+++ resources/views/work_times/create.blade.php
@@ -13,11 +13,9 @@
         <div class="form-group">
             <label for="doctor_id">Выберите специалиста</label>
             <select class="form-control" name="doctor_id">
-                <select class="form-control" name="doctor_id">
                     @foreach($doctors as $doctor)
-                        <option value="{{ $doctor->id }}" {{ $selectedDoctorId === $doctor->id ? 'selected' : '' }}>{{ $doctor->doctor_name }}</option>
+                        <option value="{{ $doctor->id }}" {{ (int) $selectedDoctorId === $doctor->id ? 'selected' : '' }}>{{ $doctor->doctor_name }}</option>
                     @endforeach
-                </select>
 
             </select>
 
