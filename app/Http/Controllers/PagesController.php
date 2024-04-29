bd46d2459d2aec5a62f461b283be62033cdfd1a0
YuraL0ns <yuralons@yandex.ru>
2024-04-29 14:40:55 +0300

    Update (Add doctor times Style)

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Artesaos\SEOTools\Facades\SEOMeta;
+use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\Http\Request;
 
 use App\Models\File;
@@ -12,6 +14,10 @@ class PagesController extends Controller
     public function show($pageAlias)
     {
         $page = Page::where('page_alias', $pageAlias)->with('getFiles')->first();
+
+        SEOTools::setTitle($page->page_title .' - '. env('APP_NAME'));
+        SEOTools::setDescription($page->page_title);
+        SEOTools::jsonLd()->addImage(env('APP_URL') .' / ' .'template/navbar/logo.png');
 
         return view('tmp.sait.page.custom', compact('page'));
     }
