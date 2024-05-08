7b3356cd73bcf91ed3fbed6060a88bbdcc5fd11d
YuraL0ns <62638148+YuraL0ns@users.noreply.github.com>
2024-05-08 18:01:22 +0300

    Update list.blade.php

--- resources/views/admin/page/messages/list.blade.php
+++ resources/views/admin/page/messages/list.blade.php
@@ -1,13 +1,17 @@
 @extends('adminlte::page')
 
-@section('title', 'Submissions')
+@section('title', 'Форма обратной связи')
 
 @section('content_header')
-    <h1>Submissions</h1>
+    <h1>Обратная связь</h1>
 @stop
 
 @section('content')
+<div class="row my-2">
+    <input class="btn btn-xs btn-primary" value="Обновить данные" onclick="document.location.reload(true)" />
+</div>
     <div class="row">
+        
         @php
             $groups = ['header' => [], 'footer' => [], 'page_vacancy' => []];
             foreach ($submissions as $submission) {
@@ -15,14 +19,16 @@
             }
         @endphp
 
+            
+
         @foreach ($groups as $type => $groupSubmissions)
-            <div class="col-md-4">
+            <div class="col-md-4 ">
                 <div class="card">
                     <div class="card-header">
                         <h3 class="card-title">{{ ucfirst($type) }} </h3>
                     </div>
                     <div class="card-body p-0">
-                        <table class="table table-striped">
+                        <table id="headerTable" class="table table-striped ">
                             <thead>
                             <tr>
                                 <th>Имя</th>
@@ -32,10 +38,10 @@
                             </thead>
                             <tbody>
                             @foreach ($groupSubmissions as $submission)
-                                <tr>
+                                <tr >
                                     <td>{{ $submission->name }}</td>
                                     <td><a href="tel:{{ $submission->phone }}">{{ $submission->phone }}</a></td>
-                                    <td>{{ $submission->created_at }}</td>
+                                    <td>{{Carbon::parse($submission->created_at)->format('d F Y г H:i')}}</td>
                                 </tr>
                             @endforeach
                             </tbody>
@@ -45,4 +51,13 @@
             </div>
         @endforeach
     </div>
+@stop
+
+@section('myJs')
+<script>
+    setTimeout(() => {
+    location.reload()
+}, 80000);
+</script>
+
 @stop
